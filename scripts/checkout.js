fbf1ef8c839e97e98bfdbfbcd6658572b5ff49dc
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2023-10-11 14:26:58 -0400

    Delete products from the cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -13,7 +13,6 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   })
-  console.log(matchingProduct);
 
 
 
@@ -42,7 +41,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id='${matchingProduct.id}'>
             Delete
           </span>
         </div>
@@ -96,5 +95,13 @@ cart.forEach((cartItem) => {
   </div>
   `;
 });
+console.log(cart)
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-console.log(cartSummaryHTML)
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', ()=>{
+      const productID = link.dataset.productId
+      removeFromCart(productId);
+
+    })
+  });
