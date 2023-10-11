9c71989fb7b203b12aec46bccd3b1c6516b40d10
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2023-10-11 14:35:01 -0400

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
 
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${productId}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -100,8 +100,12 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', ()=>{
-      const productID = link.dataset.productId
+      const productId = link.dataset.productId
       removeFromCart(productId);
+      const container =document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
 
     })
   });
